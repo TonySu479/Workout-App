cd44c3d9bf58d58b64c06ef7bdf6a6ae0c35d6b7
TonySu479 <tonysu479@gmail.com>
2021-01-28 17:24:37 +0100

    Date History

--- app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
@@ -2,6 +2,10 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sevenminuteworkout.SqliteOpenHelper
 import kotlinx.android.synthetic.main.activity_b_m_i.*
 import kotlinx.android.synthetic.main.activity_history.*
 
@@ -18,6 +22,27 @@ class HistoryActivity : AppCompatActivity() {
         }
         toolbar_history_activity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        getAllCompletedDates()
+    }
+
+    private fun getAllCompletedDates(){
+        val dbHandler = SqliteOpenHelper(this, null)
+        val allCompletedDatesList = dbHandler.getAllCompletedDatesList()
+
+        if(allCompletedDatesList.size > 0){
+            tvHistory.visibility = View.VISIBLE
+            rvHistory.visibility = View.VISIBLE
+            tvNoDataAvailable.visibility = View.GONE
+
+            rvHistory.layoutManager = LinearLayoutManager(this)
+            val historyAdapter = HistoryAdapter(this, allCompletedDatesList)
+            rvHistory.adapter = historyAdapter
+        } else {
+            tvHistory.visibility = View.GONE
+            rvHistory.visibility = View.GONE
+            tvNoDataAvailable.visibility = View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/example/a7minuteworkout/SqliteOpenHelper.kt
+++ app/src/main/java/com/example/a7minuteworkout/SqliteOpenHelper.kt
@@ -42,6 +42,19 @@ class SqliteOpenHelper(
         db.close()
     }
 
+    fun getAllCompletedDatesList() : ArrayList<String>{
+        val list = ArrayList<String>()
+        val db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM $TABLE_HISTORY", null)
+
+        while (cursor.moveToNext()) {
+            val dateValue = (cursor.getString(cursor.getColumnIndex(COLUMN_COMPLETED_DATE)))
+            list.add(dateValue)
+        }
+        cursor.close()
+        return list
+    }
+
     companion object {
         private const val DATABASE_VERSION = 1
         private const val DATABASE_NAME = "SevenMinutesWorkout.db"
